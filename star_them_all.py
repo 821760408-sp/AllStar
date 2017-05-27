5521a374420489d8461e7ff527053df7e5a0a6e3
Dr. Octo Noto <dr.octonoto@Yenius-iMac.local>
2017-05-26 20:24:12 -0400

    ADD option to star repos of a single user

--- star_them_all.py
+++ star_them_all.py
@@ -2,12 +2,15 @@ from __future__ import print_function
 from getpass import getpass
 from functools import partial
 from multiprocessing.dummy import Pool as ThreadPool
-from github import Github
+from github import Github, AuthenticatedUser
+from github.GithubException import UnknownObjectException
 import sys
 import argparse
 
 
+FOLLOW_ALL = True
 NUM_THREADS = 1
+
 
 def check_positive(value):
     ivalue = int(value)
@@ -16,9 +19,13 @@ def check_positive(value):
             'negative value {} is invalid'.format(value))
     return ivalue
 
+
 def get_arguments():
     parser = argparse.ArgumentParser(
         description='Star all repos from people you follow.')
+    parser.add_argument(
+        '--user',
+        help='name of the user whose repos you wanna star.')
     parser.add_argument(
         '--threads',
         type=check_positive,
@@ -26,13 +33,16 @@ def get_arguments():
         help='number of threads (default = 1)')
     return parser.parse_args()
 
+
 def star_them_all(repos, user):
+    assert isinstance(user, AuthenticatedUser.AuthenticatedUser)
     for repo in repos:
         # COMMENT OUT FOR SAVING API CALLS
         # if not user.has_in_starred(repo):
         # TODO: cache starred repo to guard agianst API rate limit
         print('staring repo: {}'.format(repo.full_name))
         user.add_to_starred(repo)
+
 
 def main():
     args = get_arguments()
@@ -50,26 +60,38 @@ def main():
         sys.exit(1)
 
     # Get user
-    user = g.get_user()
+    me = g.get_user()
 
-    # Get user's followings
-    following = user.get_following()
+    if args.user:
+        whosname = args.user
+        FOLLOW_ALL = False
 
-    # Get all public repos of all users
-    repos_of_all = []
-    for f in following:
-        repos_of_all.append(f.get_repos())
+    if FOLLOW_ALL:
+        # Get user's followings
+        following = me.get_following()
 
-    # Create partial method
-    partial_star = partial(star_them_all, user=user)
+        # Get all public repos of all users
+        repos_of_all = []
+        for f in following:
+            repos_of_all.append(f.get_repos())
 
-    # Get the pool of workers
-    pool = ThreadPool(n_threads)
-    # Star all repos of all followings
-    res = pool.map(partial_star, repos_of_all)
-    # Close the pool
-    pool.close()
-    pool.join()
+        # Create partial method
+        partial_star = partial(star_them_all, user=me)
+
+        # Get the pool of workers
+        pool = ThreadPool(n_threads)
+        # Star all repos of all followings
+        _ = pool.map(partial_star, repos_of_all)
+        # Close the pool
+        pool.close()
+        pool.join()
+    else:
+        try:
+            who = g.get_user(whosname)
+            repos = who.get_repos()
+            star_them_all(repos, me)
+        except UnknownObjectException as e:
+            print('The user you are trying to follow does not exist.')
 
 
 if __name__ == '__main__':
